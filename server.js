f61078a5f950e10da9bd71e30d3896a49ed0c8b7
Alison Moura <alison.oghino@gmail.com>
2016-12-05 08:16:59 -0300

    Authentication finished

--- server.js
+++ server.js
@@ -22,18 +22,73 @@ app.get('/', function (req, res) {
 
 app.get('/setup', function (req, res) {
 
-    var nick = new User({
-        name: 'Nick Cerminara',
-        password: 'password',
+    var user = new User({
+        name: 'admin',
+        password: '123',
         admin: true
     });
 
-    nick.save(function (err) {
+    user.save(function (err) {
         if (err) throw err;
         console.log('User saved successfully');
         res.json({ success: true });
     });
 });
+
+app.post('/authenticate', function (req, res) {
+    User.findOne({
+        name: req.body.name
+    }, function (err, user) {
+        if (err) throw err;
+        if (!user) {
+            res.status(400).json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+            if (user.password != req.body.password) {
+                res.status(400).json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+                var token = jwt.sign(user, app.get('superSecret'), { expiresIn: 60 });
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
+
+var apiRoutes = express.Router();
+
+apiRoutes.use('/*', function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.status(403).json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
+});
+
+apiRoutes.get('/', function (req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+apiRoutes.get('/users', function (req, res) {
+    User.find({}, function (err, users) {
+        res.json(users);
+    });
+});
+
+app.use('/api', apiRoutes);
 
 app.listen(port);
 console.log('Server is running in http://localhost:' + port);
